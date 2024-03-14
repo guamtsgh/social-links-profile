2625f84cd171b0770ad2326e5005477b5bfe9046
Guer <maythee.srianut@gmail.com>
2024-03-14 19:06:35 +0700

    finish the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import avatar from './assets/images/avatar-jessica.jpeg'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section id="container">
+      <img src={avatar} alt='avatar' />
+
+      <div id='details'>
+        <h1>Jessica Randall</h1>
+        <p>London, United Kingdom</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <p id='career'>"Front-end developer and avid teacher."</p>
+
+      <div id='buttons'>
+        <button>Github</button>
+        <button>Frontend Mentor</button>
+        <button>Linkedin</button>
+        <button>Twitter</button>
+        <button>Instagram</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  ) 
+  
 }
 
-export default App
+export default App;
